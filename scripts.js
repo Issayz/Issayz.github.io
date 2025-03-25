936f8b75c69e675d1da5bfd9024ef2087d228837
Rai <167530349+Issayz@users.noreply.github.com>
2025-03-25 08:52:27 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+
+const key = "d8939f898492c70ac214cca730b8b3ad"
+
+
+function cliqueNoBotao () {
+    const input = document.querySelector(".input-cidade").value
+
+     console.log(input)
+        alert("Botão clicado!");
+    }
+    
